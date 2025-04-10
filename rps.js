78e7e5fe12f845759c4a3a1b74b4cb195f5e0370
tendar <gtgtendar@gmail.com>
2025-04-10 13:40:26 +0530

    create player move

--- rps.js
+++ rps.js
@@ -1,6 +1,7 @@
+let computerMove = '';
 function pickComputerMove (){
   const randomNumbers = Math.random();
-  let computerMove = '';
+  
   if(randomNumbers > 0 && randomNumbers < 1/3){
     computerMove = 'rock';
   } else if (randomNumbers > 1/3 && randomNumbers < 2/3){
@@ -9,4 +10,39 @@ function pickComputerMove (){
     computerMove = 'scissor';
   }
 }
+
+function playGame (playerMove) {
+
+  pickComputerMove();
+  let result = '';
+    if (playerMove === 'rock'){
+        if (computerMove === 'rock'){
+          result = 'tie'
+        } else if (computerMove === 'paper'){
+          result = 'loose'
+        } else if (computerMove === 'scissor'){
+          result = 'win'
+        }
+    } else if (playerMove === 'paper'){
+        if (computerMove === 'rock'){
+          result = 'win'
+        } else if (computerMove === 'paper'){
+          result = 'tie'
+        } else if (computerMove === 'scissor'){
+          result = 'loose'
+        }
+    } else if (playerMove === 'scissor') {
+      if (computerMove === 'rcok'){
+        result = 'loose'
+      }else if (computerMove === 'paper'){
+        result = 'win'
+      } else if (computerMove === 'scissor') {
+        result = 'tie'
+      }
+    }
+
+    alert(`you pick: ${playerMove} computer pick: ${computerMove} result: ${result}`);
+}
+
+
 
